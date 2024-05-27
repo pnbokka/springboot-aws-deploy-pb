f4c872ecdab0f6aa88362f00fa98d48733b35f12
Praveen Bokka <praveen.bokka@google.com>
2024-05-27 11:40:59 -0400

    Second commit

---
+++ src/main/java/com/example/springboot_aws_deploy_pb/SpringbootAwsDeployPbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws_deploy_pb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsDeployPbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsDeployPbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_aws_deploy_pb/SpringbootAwsDeployPbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws_deploy_pb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDeployPbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
